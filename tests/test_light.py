e803ed7c8a3270705cbe6328bfd32d6e8fb865c0
Decaux Nicolas <decauxnico@gmail.com>
2025-03-03 22:39:32 +0100

    Update test_light.py

--- tests/test_light.py
+++ tests/test_light.py
@@ -82,7 +82,7 @@ async def test_light_entity_initialization(hass, mock_socket_module):
     _LOGGER.debug("Test de l'unique_id:")
     _LOGGER.debug("- Attendu: '%s'", expected_unique_id)
     _LOGGER.debug("- Obtenu: '%s'", entity.unique_id)
-    assert entity.unique_id == expected_unique_id
+    assert str(entity.unique_id) == str(expected_unique_id)
 
     # Test des attributs de base
     _LOGGER.debug("Test des attributs - Name: %s, Device IP: %s", entity.name, entity.device_ip)
